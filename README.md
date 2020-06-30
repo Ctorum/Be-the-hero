b24a602ce04992dfb2515a5d727f1ac6f2782f90
Ctorum <vhcabralrezende@hotmail.com>
2020-06-30 15:41:24 -0300

    v0.5.2

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Be the hero project by Ctorum<br/>
 
 ### yarn start
     run this to view in your browser(localhost:3000)
+    run this in backend file to start the server
--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Container } from './styles';
+import api from '../../services/api';
 import logo from '../../assets/logo.png';
 import heroes from '../../assets/heroes.png';
 import icon from '../../assets/icon.svg';
 
 function Signin() {
+    const [id, setId] = useState('');
+
+    async function handleCreate() {
+        const data = {
+            id,
+        };
+
+        const response = await api.post('/sessions', data);
+        console.log(data);
+        console.log(response);
+    }
     return (
         <Container>
             <div className="login">
                 <img src={logo} alt="logo" />
                 <label htmlFor="user">Faça seu logon</label>
-                <input type="text" id="user" placeholder="Sua ID" />
-                <a className="link1" href="# ">
+                <input
+                    type="text"
+                    id="user"
+                    placeholder="Sua ID"
+                    value={id}
+                    onChange={(v) => setId(v.target.value)}
+                />
+                <a className="link1" href="# " onClick={handleCreate}>
                     Entrar
                 </a>
                 <div className="iconBtn">
