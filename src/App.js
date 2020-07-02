4b9b5006083e1f0f6293ed1e230089acfbdc9ed3
Ctorum <vhcabralrezende@hotmail.com>
2020-07-02 11:48:23 -0300

    v1.0.1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-// import Signin from './pages/Signin;
-// import Register from './pages/Register';
 import Routes from './routes';
 import { GlobalStyle } from './pages/Register/styles';
 
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import { Container } from './styles';
+import api from '../../services/api';
 import logo from '../../assets/logo.png';
 import off from '../../assets/off.svg';
 
 function Profile() {
+    const [list, setList] = useState([]);
     const history = useHistory();
+    const UserId = localStorage.getItem('UserId');
+
+    async function handleList() {
+        const response = await api.get('/profile', {
+            headers: { Authorization: UserId },
+        });
+        setList(response.data);
+    }
+
+    useEffect(() => {
+        handleList();
+    }, []);
+
+    const listElements = list.map(function (item, index) {
+        return `${index}:${item.title}`;
+    });
+    console.log(listElements);
+    console.log(list);
 
     return (
         <Container>
@@ -32,7 +52,15 @@ function Profile() {
                 <div className="content">
                     <div className="leftDiv">
                         <h2>Casos cadastrados</h2>
-                        <div className="elementsList" />
+                        <div className="elementsList">
+                            {list.map((item) => (
+                                <>
+                                    <h1>{item.title}</h1>
+                                    <p>{item.description}</p>
+                                    <p>Valor: R${item.value}</p>
+                                </>
+                            ))}
+                        </div>
                     </div>
                     <div className="rightDiv" />
                 </div>
